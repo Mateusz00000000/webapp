aecc0baa6b244a0d5824e87741b187bbfe1f5063
Mateusz00000000 <111456525+Mateusz00000000@users.noreply.github.com>
2022-11-22 13:26:18 -0600

    Nov. 22

--- scripts.js
+++ scripts.js
@@ -26,14 +26,52 @@ function hide(){
 
    
 
-   //POST-IT NOTE (JAVASCRIPT)
-if (localStorage["note"]) {
-    var note = localStorage["note"];
-    document.getElementById("note").value = note;
-   }
-   
-   function autoSave() {
-    var note = document.getElementById("note").value;
-    localStorage.setItem("note", note);
-   }
+
+//JACKIE'S NOTES
+if (localStorage["monday"]) {
+    var monday = localStorage["monday"];
+    document.getElementById("monday").value = monday;
+  } 
+  function saveMonday() {
+    var monday = document.getElementById("monday").value;
+    localStorage.setItem("monday", monday);
+  }
+  
+  if (localStorage["tuesday"]) {
+    var tuesday = localStorage["tuesday"];
+    document.getElementById("tuesday").value = tuesday;
+  }
+  function saveTuesday() {
+    var tuesday = document.getElementById("tuesday").value;
+    localStorage.setItem("tuesday", tuesday);
+  }
+  
+  if (localStorage["wednesday"]) {
+    var wednesday = localStorage["wednesday"];
+    document.getElementById("wednesday").value = wednesday;
+  }
+  function saveWednesday() {
+    var wednesday = document.getElementById("wednesday").value;
+    localStorage.setItem("wednesday", wednesday);
+  }
+  
+  if (localStorage["thursday"]) {
+    var thursday = localStorage["thursday"];
+    document.getElementById("thursday").value = thursday;
+  }
+  function saveThursday() {
+    var thursday = document.getElementById("thursday").value;
+    localStorage.setItem("thursday", thursday);
+  }
+  
+  if (localStorage["friday"]) {
+    var friday = localStorage["friday"];
+    document.getElementById("friday").value = friday;
+  }
+  function saveFriday() {
+    var friday = document.getElementById("friday").value;
+    localStorage.setItem("friday", friday);
+  }
+
+
    
